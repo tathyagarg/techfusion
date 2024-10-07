5ee2a495b5254db3b4801f77a1336bbb9aba3d44
Tathya Garg <tathya.garg@gmail.com>
2024-10-07 13:36:42 +0530

    Word change

--- src/pages/challenges/challenge-5.js
+++ src/pages/challenges/challenge-5.js
@@ -5,7 +5,7 @@ export default function Challenge5() {
     return <>
         <div className="content">
             <div className="rules-container">
-                <p style={{textAlign: "center"}}>The answer lies in a shift of vision</p>
+                <p style={{textAlign: "center"}}>The password lies in a shift of vision</p>
                 <img src={hidden_word} width={"100%"}></img>
                 <a href={flag} download style={{color: "white"}}>Click to download your next clue</a>
             </div>
